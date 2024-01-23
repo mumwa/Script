6c40e096a59335ff53c12e7ec86d9324732523da
mumwa <mumwa@naver.com>
2024-01-23 22:43:35 +0900

    [ Feat ] make directory for sources

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
 const { SEND_MAIN_PING } = require("./constants");
+const fs = require("fs");
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -17,6 +18,19 @@ function createWindow() {
 ipcMain.on(SEND_MAIN_PING, (event, arg) => {
   console.log("Main.js received a ping!!!");
 });
+
+//디렉토리를 만듬
+let directoryPath = app.getPath("userData");
+console.log("directoryPath:", directoryPath);
+
+try {
+  !fs.existsSync(`${directoryPath}/src`) &&
+    fs.mkdirSync(`${directoryPath}/src`);
+  console.log("making direcotry success!");
+} catch (err) {
+  console.error(err);
+}
+
 app.whenReady().then(() => {
   createWindow();
 });
