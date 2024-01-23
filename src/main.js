f0370c33dc1965b6be3ff7cf7ebc8f24e5c826d6
mumwa <mumwa@naver.com>
2024-01-23 22:44:56 +0900

    [ Feat ] Make json file Temporary code for test

--- src/main.js
+++ src/main.js
@@ -31,6 +31,34 @@ try {
   console.error(err);
 }
 
+let array = [
+  {
+    speed_scaler: "10000",
+    very_slow_walk_speed: "4470",
+    slow_walk_speed: "8940",
+    walk_speed: "13410",
+    fast_walk_speed: "17880",
+  },
+  {
+    speed_scaler: "5000",
+    very_slow_walk_speed: "1234",
+    slow_walk_speed: "6666",
+    walk_speed: "7777",
+    fast_walk_speed: "23000",
+  },
+];
+
+try {
+  if (fs.existsSync(`${directoryPath}/src/test.json`)) {
+    console.log("json 파일이 이미 있음");
+  } else {
+    fs.writeFileSync(`${directoryPath}/src/test.json`, JSON.stringify(array));
+    console.log("json 파일 작성");
+  }
+} catch (err) {
+  console.error(err);
+}
+
 app.whenReady().then(() => {
   createWindow();
 });
