ae463d066f4c81a1d3ec59b93356aeaab8444135
mumwa <mumwa@naver.com>
2024-01-24 17:33:27 +0900

    [ Feat ] Remove App.js component

--- src/App.js
+++ src/App.js
@@ -9,19 +9,7 @@ function App() {
   };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello world!!!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button onClick={sendMain}>Send Ping</button>
-      </header>
+      <button onClick={sendMain}>Send Ping</button>
     </div>
   );
 }
