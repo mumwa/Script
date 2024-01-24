68d32552d399e9926bb409b2cd8f945da51ca696
mumwa <mumwa@naver.com>
2024-01-24 17:39:11 +0900

    [ Feat ] ipcRenderer. channel to recieve message from main temporary code for test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { SEND_MAIN_PING } from "./channels";
+import { SEND_MAIN_PING, SEND_RENDERER_PING } from "./channels";
 
 function App() {
   const { ipcRenderer } = window.require("electron");
   const sendMain = () => {
     ipcRenderer.send(SEND_MAIN_PING, "send");
   };
+
+  ipcRenderer.on(SEND_RENDERER_PING, (event, payload) => {
+    console.log(payload);
+  });
+
   return (
     <div className="App">
       <button onClick={sendMain}>Send Ping</button>
